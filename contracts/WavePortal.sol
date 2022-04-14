576b09899a3688df863f8ff3c6248b97c0a04bb8
Carlos Chapa <bravelittletodolist@gmail.com>
2022-04-14 15:16:45 -0600

    added wave and getWave functions and reflected those functions over to JS

--- contracts/WavePortal.sol
+++ contracts/WavePortal.sol
@@ -5,7 +5,21 @@ pragma solidity ^0.8.4;
 import "hardhat/console.sol";
 
 contract WavePortal {
+
+    uint totalWaves;
+
     constructor(){
         console.log("YOOOO IT'S YA BOOYY.");
     }
+
+    function wave() public {
+        totalWaves += 1;
+        console.log("%s has waved!", msg.sender);
+    }
+
+    function getTotalWaves() public view returns (uint256){
+        console.log("We have %d total waves!", totalWaves);
+        return totalWaves;
+    }
+
 }
